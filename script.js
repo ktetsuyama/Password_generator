b6fe82254c71bafa3ce07341a260fbc64703dca2
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2023-12-30 13:59:50 -0800

    started coding

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
