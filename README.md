8b950e7237267ae35ecd15ea79406db41f453b9b
Keegan Royal-Eisenberg <kroyal.eisenberg@gmail.com>
2024-01-07 22:43:13 -0800

    Added a quit feature at the beginning, cleaned up the prompt messages a bit, and updated the README with screenshots and a working link to the application.

--- README.md
+++ README.md
@@ -30,15 +30,19 @@ I created the javascript necessary to generate a password based on user selected
 
 The client is prompted to choose whether they would like each of the options and a random password is created based upon their selections. If the client declines to use an option, they are allowed to move forward but are encouraged to choose options that will help their password be more secure.
 
-![Screenshot of the password prompts](assets/screenshot.png)
+![Screenshot of the password prompts](./assets/images/digits_prompt.png)
 
-[Link to the deployed application](https://ktetsuyama.github.io/Janken/)
+![Screenshot of the generated password](./assets/images/generated_password.png)
+
+[Link to the deployed application](https://ktetsuyama.github.io/Password_generator/)
 
 ## Credits
 
 I collaborated George Schultz, Aster Oliva, Meera G K, and Aileen Ngyuen to troubleshoot any bugs that I had during development of this application. I also used phind.com to understand why my variables weren't being called. 
 
 Tutorials used:
+
+[Stackoverflow post about allowing the user to quit a javascript window](https://stackoverflow.com/questions/12864582/javascript-prompt-cancel-button-to-terminate-the-function)
 
 ## License
 
--- assets/script.js
+++ assets/script.js
@@ -25,7 +25,9 @@ function generatePassword() {
 	var passLength = prompt("How many characters in length would you like your password to be? \nPlease enter a numerical value between 8 - 128. \n(hint: longer passwords are more secure passwords.)");
 
 	// is it between 8-128 characters?
-	if (passLength < 8 || passLength > 128) {
+	if (passLength === null) {
+		return;
+	} else if (passLength < 8 || passLength > 128) {
 		alert("Please choose a number of characters that is at least 8 but not more the 128");
 		return generatePassword();
 	} else if (isNaN(passLength)) {
@@ -36,35 +38,35 @@ function generatePassword() {
 	}
 
 	// ask if they want uppercase
-	var choicesUpp = confirm("Would you like to include Upper case letters in your password? \n(hint: a variety of different characters, including upper case letters helps create a more secure password.)");
+	var choicesUpp = confirm("Would you like to include Upper case letters in your password? \n\n(hint: a variety of different characters, including upper case letters helps create a more secure password.)");
 
 	if (choicesUpp) {
-		alert("Thank you. Your password will include upper case letters. \nPlease choose another criteria after clicking 'Ok'.");
+		alert("Thank you. \nYour password will include upper case letters. \nPlease choose another criteria after clicking 'Ok'.");
 	} else {
 		alert("Ok, your password will not include upper case letters, but your password might not be very secure.");
 	}
 
 	// ask if the want lowercase
-	var choicesLow = confirm("Would you like to include Lower case letters in your password? \n(a variety of different characters, including lower case letters helps create a more secure password.)");
+	var choicesLow = confirm("Would you like to include Lower case letters in your password? \n\n(hint: a variety of different characters, including lower case letters helps create a more secure password.)");
 
 	if (choicesLow) {
-		alert("Thank you. Your password will include lower case letters.  Please choose another criteria after clicking 'Ok'.");
+		alert("Thank you. \nYour password will include lower case letters.  Please choose another criteria after clicking 'Ok'.");
 	} else {
 		alert("Ok, your password will not include lower case letters, but your password might not be very secure.");
 	}
 	// ask if they want digits
-	var choicesNum = confirm("Would you like to include numerical digits in your password? \n(a variety of different characters, including numerical digits helps create a more secure password.)");
+	var choicesNum = confirm("Would you like to include numerical digits in your password? \n\n(hint: a variety of different characters, including numerical digits helps create a more secure password.)");
 
 	if (choicesNum) {
-		alert("Thank you. Your password will include numerical digits. Please choose another criteria after clicking 'Ok'.");
+		alert("Thank you. \nYour password will include numerical digits. Please choose another criteria after clicking 'Ok'.");
 	} else {
 		alert("Ok, your password will not include numerical digits, but your password might not be very secure.");
 	}
 	// ask if they want special characters
-	var choicesSpec = confirm("Would you like to include Special characters in your password? \ne.g., \" !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~\" \n(hint: a variety of different characters, including Special characters helps create a more secure password.)");
+	var choicesSpec = confirm("Would you like to include Special characters in your password? \ne.g.,  !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~ \n\n(hint: a variety of different characters, including Special characters helps create a more secure password.)");
 
 	if (choicesSpec) {
-		alert("Thank you. Your password will include special characters.");
+		alert("Thank you. \nYour password will include special characters.");
 	} else {
 		alert("Ok, your password will not include special characters, but your password might not be very secure.");
 	}
